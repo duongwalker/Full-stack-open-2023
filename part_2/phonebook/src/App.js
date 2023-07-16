decda8c7db4a2bc370584e9fe05eb7c69cbc0d35
Duong Hoang <duong.hoang@tuni.fi>
2023-07-16 18:12:11 +0300

    try update part 2 for commit

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     const max = 999999
     const id = Math.floor(Math.random() * (max - min + 1)) + min
     return id
-}
+  }
 
   useEffect(() => {
     personService
@@ -35,43 +35,50 @@ const App = () => {
     const personObject = {
       name: newName.trim(),
       number: newNumber.trim(),
-      id: generateId(),
     }
     const comparisons = persons.map((person) => {
-      const areEqual=isEqual(person.name.trim(), newName.trim())
+      const areEqual = isEqual(person.name.trim(), newName.trim())
       return areEqual
     })
 
-    if(comparisons.includes(true)) {
-      if(window.confirm(`${newName.trim()} is already added to phonebook, replace the old number with a new one?`)) {
+    if (comparisons.includes(true)) {
+      if (window.confirm(`${newName.trim()} is already added to phonebook, replace the old number with a new one?`)) {
         persons.map((person) => {
-          if(person.name.trim()===newName.trim()) {
-            personService
-              .update(person.id,personObject)
-              .catch(error => {
-                setIsSuccess(false)
-                setMessage(`Information of ${person.name.trim()} has already been removed from the server`)
-                setTimeout(() => {
-                  setMessage(null)
-                }, 3000)
-              })
+          if (person.name.trim() === newName.trim()) {
+            console.log('Helooooooo')
+
+
+            updateName(person.id, personObject)
           }
         })
       }
+      else {
+        setMessage(null)
+      }
     }
     else {
-      if(newName!=='' && newNumber!=='') {
-      personService
-      .create(personObject)
-      .then((response) => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
-    }
-    else {
-      alert('Please fill in the name and number!')
-    }
+      if (newName !== '' && newNumber !== '') {
+        personService
+          .create(personObject)
+          .then((response) => {
+            setPersons(persons.concat(response.data))
+            setNewName('')
+            setNewNumber('')
+          })
+          .catch(error => {
+            console.log(error.response.data.error)
+            setIsSuccess(false)
+            setMessage(error.response.data.error)
+          })
+      }
+
+      else if (newNumber.length<8) {
+        setMessage()
+      }
+
+      else {
+        alert('Please fill in the name and number!')
+      }
     }
     setNewName('')
     setNewNumber('')
@@ -79,16 +86,16 @@ const App = () => {
 
   const handleNameChange = (e) => {
     setNewName(e.target.value)
-   
+
   }
 
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value)
   }
-  
+
   const handleFilterChange = (e) => {
     e.preventDefault()
-    if(!e.target.value) {
+    if (!e.target.value) {
       setShowAll(true)
       setFilter(e.target.value)
     }
@@ -98,22 +105,48 @@ const App = () => {
     }
   }
 
-  const deleteName= (id) => {
-    const person=persons.find(person => person.id===id)
-    if(window.confirm(`Delete ${person.name.trim()}?` )) {
+  const deleteName = (id) => {
+    const person = persons.find(person => person.id === id)
+    if (window.confirm(`Delete ${person.name.trim()}?`)) {
       personService
         .remove(id)
       setPersons(persons.filter(person => person.id !== id))
     }
   }
 
-  const updateName=(id, newObject) => {
+  const updateName = (id, newObject) => {
     personService
       .update(id, newObject)
+      .then(returnedPerson => {
+        if (returnedPerson.data === null) {
+          setIsSuccess(false)
+          setMessage(`Information of ${newObject.name.trim()} has already been removed from the server`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        }
+        else {
+          console.log(returnedPerson)
+          setPersons(persons.map(person => {
+            return person.id !== id ? person : returnedPerson
+          }))
+        }
+      })
+      .catch(error => {
+        console.log(error.response.data.error)
+        setIsSuccess(false)
+        setMessage(error.response.data.error)
+      })
   }
-  
+
   const handleMessage = () => {
-    setMessage(`Added ${newName}`)
+    if (newName.trim().length === 0) {
+      setMessage(null)
+    }
+    else {
+      setIsSuccess(true)
+      setMessage(`Added ${newName}`)
+    }
     setTimeout(() => {
       setMessage(null)
     }, 5000)
@@ -123,16 +156,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      
-      <Notification message={message} isSuccess={isSuccess}/>
-      <Filter value={filter} onChange={handleFilterChange}/>
+
+      <Notification message={message} isSuccess={isSuccess} />
+      <Filter value={filter} onChange={handleFilterChange} />
       <h2>add a new</h2>
-      
-      <PersonForm  onSubmit={addName} handleMessage={handleMessage} name={newName} number={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+
+      <PersonForm onSubmit={addName} handleMessage={handleMessage} name={newName} number={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
       <div>
-        {numbersToShow.map((person) => 
-        <Persons onClick={() => deleteName(person.id)} key={person.name} name={person.name} number={person.number}/>)}
+        {numbersToShow.map((person) =>
+          <Persons onClick={() => deleteName(person.id)} key={person.name} name={person.name} number={person.number} />)}
       </div>
     </div>
   )
--- part_2/phonebook/src/services/persons.js
+++ part_2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://render-test-eegp.onrender.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
