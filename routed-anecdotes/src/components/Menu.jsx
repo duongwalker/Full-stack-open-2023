7775dcd842f6e680f0a335b9d6ea025e64bf9c4b
Duong Hoang <duong.hoang@tuni.fi>
2023-12-28 18:18:28 +0300

    7.1 to 7.3

---
+++ routed-anecdotes/src/components/Menu.jsx
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+      <div>
+        <Link href='#' style={padding} to="/">anecdotes</Link>
+        <Link href='#' style={padding} to ="/create">create new</Link>
+        <Link href='#' style={padding} to="/about">about</Link>
+      </div>
+  )
+}
+
+export default Menu
